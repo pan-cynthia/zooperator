1a3b25be578a98c1447d3f607d508183c6bacf32
Cynthia Pan <cynthiapan11@gmail.com>
2025-11-23 19:02:31 -0800

    test: add unit tests for Animal class

--- test/test_animal.cpp
+++ test/test_animal.cpp
@@ -1,7 +1,172 @@
 #include <gtest/gtest.h>
 
-// dummy test so executable builds
+#include "animal.h"
+#include "penguin.h"
 
-TEST(Dummy, Placeholder) {
-  SUCCEED();
+TEST(AnimalTest, ConstructorInitialization) {
+  Penguin penguin("Pororo", 8);
+  EXPECT_EQ(penguin.getName(), "Pororo");
+  EXPECT_EQ(penguin.getSpecies(), "Penguin");
+  EXPECT_EQ(penguin.getAge(), 8);
+  EXPECT_EQ(penguin.getHealthLevel(), 100);
+  EXPECT_EQ(penguin.getHungerLevel(), 0);
+  EXPECT_EQ(penguin.getHappinessLevel(), 100);
+  EXPECT_EQ(penguin.getEnergyLevel(), 100);
+}
+
+TEST(AnimalTest, StatsClampAt100) {
+  Penguin penguin("Pororo", 8);
+  penguin.updateHealth(50);
+  EXPECT_EQ(penguin.getHealthLevel(), 100);
+
+  penguin.updateHunger(110);
+  EXPECT_EQ(penguin.getHungerLevel(), 100);
+
+  penguin.updateHappiness(10);
+  EXPECT_EQ(penguin.getHappinessLevel(), 100);
+
+  penguin.updateEnergy(25);
+  EXPECT_EQ(penguin.getEnergyLevel(), 100);
+}
+
+TEST(AnimalTest, StatsClampAt0) {
+  Penguin penguin("Pororo", 8);
+  penguin.updateHealth(-150);
+  EXPECT_EQ(penguin.getHealthLevel(), 0);
+
+  penguin.updateHunger(-15);
+  EXPECT_EQ(penguin.getHungerLevel(), 0);
+
+  penguin.updateHappiness(-125);
+  EXPECT_EQ(penguin.getHappinessLevel(), 0);
+
+  penguin.updateEnergy(-101);
+  EXPECT_EQ(penguin.getEnergyLevel(), 0);
+}
+
+TEST(AnimalTest, IsAlive) {
+  Penguin penguin("Pororo", 8);
+  EXPECT_TRUE(penguin.isAlive());
+
+  penguin.updateHealth(-100);
+  EXPECT_FALSE(penguin.isAlive());
+}
+
+TEST(AnimalTest, NeedsAttentionWhenHealthLow) {
+  Penguin penguin("Pororo", 8);
+  EXPECT_FALSE(penguin.needsAttention());
+
+  penguin.updateHealth(-90);  // health = 10
+  EXPECT_TRUE(penguin.needsAttention());
+}
+
+TEST(AnimalTest, NeedsAttentionWhenHungerHigh) {
+  Penguin penguin("Pororo", 8);
+  EXPECT_FALSE(penguin.needsAttention());
+
+  penguin.updateHunger(85);  // hunger = 85
+  EXPECT_TRUE(penguin.needsAttention());
+}
+
+TEST(AnimalTest, NeedsAttentionWhenHappinessLow) {
+  Penguin penguin("Pororo", 8);
+  EXPECT_FALSE(penguin.needsAttention());
+
+  penguin.updateHappiness(-82);  // happiness = 18
+  EXPECT_TRUE(penguin.needsAttention());
+}
+
+TEST(AnimalTest, NeedsAttentionWhenEnergyLow) {
+  Penguin penguin("Pororo", 8);
+  EXPECT_FALSE(penguin.needsAttention());
+
+  penguin.updateEnergy(-99);
+  EXPECT_TRUE(penguin.needsAttention());
+}
+
+TEST(AnimalTest, DoesNotNeedAttention) {
+  Penguin penguin("Pororo", 8);
+  penguin.updateHealth(-50);     // health = 50
+  penguin.updateHunger(40);      // hunger = 40
+  penguin.updateHappiness(-30);  // happiness = 70
+  penguin.updateEnergy(-15);     // energy = 85
+  EXPECT_FALSE(penguin.needsAttention());
+}
+
+TEST(AnimalTest, EatingReducesHunger) {
+  Penguin penguin("Pororo", 8);
+  penguin.updateHunger(50);  // make hungry
+  penguin.eat(20);
+  EXPECT_EQ(penguin.getHungerLevel(), 30);
+}
+
+TEST(AnimalTest, EatingIncreasesHappiness) {
+  Penguin penguin("Pororo", 8);
+  penguin.updateHappiness(-30);  // happiness = 70
+  penguin.eat(20);
+  EXPECT_EQ(penguin.getHappinessLevel(), 75);  // 70 + 5
+}
+
+TEST(AnimalTest, EatingZeroAmount) {
+  Penguin penguin("Pororo", 8);
+  penguin.updateHunger(50);
+
+  int initial_hunger = penguin.getHungerLevel();
+  penguin.eat(0);
+  EXPECT_EQ(penguin.getHungerLevel(), initial_hunger);
+}
+
+TEST(AnimalTest, EatingNegativeAmount) {
+  Penguin penguin("Pororo", 8);
+  penguin.updateHunger(40);
+
+  int initial_hunger = penguin.getHungerLevel();  // hunger = 40
+  penguin.eat(-10);
+  EXPECT_EQ(penguin.getHungerLevel(), initial_hunger);
+}
+
+TEST(AnimalTest, SleepingRestoresEnergy) {
+  Penguin penguin("Pororo", 8);
+  penguin.updateEnergy(-70);  // energy = 30
+
+  penguin.sleep(5);
+  EXPECT_EQ(penguin.getEnergyLevel(), 80);
+}
+
+TEST(AnimalTest, SleepingRestoresHealth) {
+  Penguin penguin("Pororo", 8);
+  penguin.updateHealth(-15);  // health = 85
+
+  penguin.sleep(3);
+  EXPECT_EQ(penguin.getHealthLevel(), 88);
+}
+
+TEST(AnimalTest, SleepingIncreasesHunger) {
+  Penguin penguin("Pororo", 8);
+  penguin.updateHunger(25);  // hunger = 25
+
+  penguin.sleep(10);
+  EXPECT_EQ(penguin.getHungerLevel(), 45);
+}
+
+TEST(AnimalTest, SleepingZeroHours) {
+  Penguin penguin("Pororo", 8);
+  int initial_energy = penguin.getEnergyLevel();
+  int initial_health = penguin.getHealthLevel();
+  int initial_hunger = penguin.getHungerLevel();
+
+  penguin.sleep(0);
+
+  EXPECT_EQ(penguin.getEnergyLevel(), initial_energy);
+  EXPECT_EQ(penguin.getHealthLevel(), initial_health);
+  EXPECT_EQ(penguin.getHungerLevel(), initial_hunger);
+}
+
+TEST(AnimalTest, SleepingNegativeHours) {
+  Penguin penguin("Pororo", 8);
+  int initial_energy = penguin.getEnergyLevel();
+
+  penguin.sleep(-5);
+
+  EXPECT_EQ(penguin.getEnergyLevel(), initial_energy);
 }
