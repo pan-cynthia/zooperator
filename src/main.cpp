3af07fcd8b07de837ecce713837e62e3f99d57b1
Cynthia Pan <cynthiapan11@gmail.com>
2025-12-03 12:33:41 -0800

    feat: implement main game loop in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <string>
+
+#include "game.h"
+#include "player.h"
 
 int main() {
-  std::cout << "Zooperator running!\n";
+  std::string player_name;
+  std::cout << "Hi! Please enter your name: ";
+  std::getline(std::cin, player_name);
+  Player player(player_name);
+
+  std::string zoo_name;
+  std::cout << "Give your zoo a name: ";
+  std::getline(std::cin, zoo_name);
+  Game game(player, zoo_name);
+
+  game.start();
+
   return 0;
 }
