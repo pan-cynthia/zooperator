fa26150b94822f274bb9d3bbaed2ceca69e1ed3a
Cynthia Pan <cynthiapan11@gmail.com>
2025-12-08 12:15:59 -0800

    feat: enforce non empty player and zoo names

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,26 @@
 
 int main() {
   std::string player_name;
-  std::cout << "Hi! Please enter your name: ";
-  std::getline(std::cin, player_name);
+  do {
+    std::cout << "Hi! Please enter your name: ";
+    std::getline(std::cin, player_name);
+
+    if (player_name.empty()) {
+      std::cout << "Name cannot be empty. Please try again.\n";
+    }
+  } while (player_name.empty());
+
   Player player(player_name);
-
   std::string zoo_name;
-  std::cout << "Give your zoo a name: ";
-  std::getline(std::cin, zoo_name);
-  Game game(player, zoo_name);
+  do {
+    std::cout << "Give your zoo a name: ";
+    std::getline(std::cin, zoo_name);
+    if (zoo_name.empty()) {
+      std::cout << "Zoo name cannot be empty. Please try again.\n";
+    }
+  } while (zoo_name.empty());
 
+  Game game(player, zoo_name);
   game.start();
 
   return 0;
