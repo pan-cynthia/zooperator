30a3e7a1a018057e30e412828d2582be38f00f6b
Cynthia Pan <cynthiapan11@gmail.com>
2025-12-08 19:29:56 -0800

    test: add unit tests for Monkey class

---
+++ test/test_monkey.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+#include "monkey.h"
+
+TEST(MonkeyTest, ConstructorInitialization) {
+  Monkey monkey("George", 3);
+  EXPECT_EQ(monkey.getName(), "George");
+  EXPECT_EQ(monkey.getSpecies(), "Monkey");
+  EXPECT_EQ(monkey.getAge(), 3);
+  EXPECT_EQ(monkey.getPurchaseCost(), 600.0);
+  EXPECT_EQ(monkey.getFeedingCost(), 15.0);
+  EXPECT_EQ(monkey.getMaintenanceCost(), 25.0);
+}
